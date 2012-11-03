a3d84e98326c0fbf96e5ef64df498b94ea9f749b
Virtaset <valtteri.virtanen@gmail.com>
2012-11-03 15:03:49 +0200

    ircbot and gitignore skeletons

---
+++ ircbot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+
+class IrcBot:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        print "connecting"
+
+    def disconnect(self):
+        pass
+
+    def handle_server_response(self, response):
+        pass
+
