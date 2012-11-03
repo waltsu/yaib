4e9af723f6b8a88091cd4ebb90a7dfb0e3ab1d48
Virtaset <valtteri.virtanen@gmail.com>
2012-11-03 15:34:35 +0200

    Client receives messages correctly

--- ircbot.py
+++ ircbot.py
@@ -1,15 +1,37 @@
 # -*- coding: utf8 -*-
 
+import socket
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger('ircbot')
+logger.setLevel(logging.DEBUG)
+
 class IrcBot:
     def __init__(self):
-        pass
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
-        print "connecting"
+    def connect(self, host, port):
+        logger.info("connecting to %s:%s" % (host, port))
+        self._socket.connect((host, port))
+        logger.info("connected")
+        self._response_loop()
 
-    def disconnect(self):
-        pass
+    def _response_loop(self):
+        def get_server_response():
+            response = ''
+            while '\r\n' not in response:
+                response += self._socket.recv(1)
 
-    def handle_server_response(self, response):
-        pass
+            return response
+            
+        while True:
+            server_response = get_server_response()
+            self._handle_server_response(server_response)
 
+    def _handle_server_response(self, response):
+        logger.debug("got response %s" % response)
+
+if __name__ == '__main__':
+    ircbot = IrcBot()
+    ircbot.connect("irc.quakenet.org", 6667)
