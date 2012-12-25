935f0062f680181b19112832ffa588b592194721
Valtteri Virtanen <valtteri@taiste.fi>
2012-12-25 18:57:45 +0200

    Experiments for handling-system

--- message_handler.py
+++ message_handler.py
@@ -1,8 +1,43 @@
+# -*- coding: utf8 -*-
 import logging
 logging.basicConfig()
 logger = logging.getLogger('ircbot')
 logger.setLevel(logging.DEBUG)
 
+import re
+
+class UnknowInputException(Exception):
+    pass
+
 class MessageHandler():
-    def handle(self, message):
-        logger.debug("Got response: %s" % message)
+    def __init__(self):
+        self._handlers = {"notice auth": self.handle_notice,
+                          "ping": self.handle_ping}
+
+    def handle(self, raw_message):
+        parsed_message = self.parse(raw_message)
+        message = parsed_message['message'].lower()
+        self._handlers[message](parsed_message['content'])
+        
+
+    def parse(self, message):
+        """
+        This might need some adjusting, when handling more complex messages. The regex and message-content pair might not be enough
+        """
+        logger.debug("Parsing message: {message}".format(message=message))
+        pattern = re.compile('(.+)\s:(.*)')
+        result = re.search(pattern, message)
+        if result:
+            parsed_message = dict()
+            parsed_message['message'] = result.group(1)
+            parsed_message['content'] = result.group(2)
+            return parsed_message
+        else:
+            raise UnknowInputException
+
+    # HANDLERS
+    def handle_notice(self, message):
+        pass
+
+    def handle_ping(self, message):
+        pass
--- tests/message_handler_tests.py
+++ tests/message_handler_tests.py
@@ -1,6 +1,15 @@
 import unittest
 
+from mock import patch
+
+from yaib.message_handler import MessageHandler
+
 class MessageHandlerTests(unittest.TestCase):
 
-    def test_message_parsing(self):
-        self.assertEqual(False, True)
+    @patch.object(MessageHandler, "handle_notice")
+    def test_notice_parsing(self, m_notice):
+        handler = MessageHandler()
+        test_message = 'NOTICE AUTH :*** Looking up your hostname'
+        handler.handle(test_message)
+        self.assertTrue(m_notice.called)
+
