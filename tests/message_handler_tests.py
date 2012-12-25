d3f3bd5435848404d5602d0ba3398187f09f8c54
Valtteri Virtanen <valtteri@taiste.fi>
2012-12-25 19:11:35 +0200

    Added ping handler skeleton

--- tests/message_handler_tests.py
+++ tests/message_handler_tests.py
@@ -5,7 +5,6 @@ from mock import patch
 from yaib.message_handler import MessageHandler
 
 class MessageHandlerTests(unittest.TestCase):
-
     @patch.object(MessageHandler, "handle_notice")
     def test_notice_parsing(self, m_notice):
         handler = MessageHandler()
@@ -13,3 +12,9 @@ class MessageHandlerTests(unittest.TestCase):
         handler.handle(test_message)
         self.assertTrue(m_notice.called)
 
+    @patch.object(MessageHandler, "handle_ping")
+    def test_ping_parsing(self, m_ping):
+        handler = MessageHandler()
+        test_message = 'PING :687010916'
+        handler.handle(test_message)
+        self.assertTrue(m_ping.called)
