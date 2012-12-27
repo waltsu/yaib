d507a9a26877da0629d7cad4d3f0bd85a7733d3a
Valtteri Virtanen <valtteri@taiste.fi>
2012-12-28 00:01:48 +0200

    Fixed wrong mock message in irc event test

--- tests/irc_event_tests.py
+++ tests/irc_event_tests.py
@@ -5,10 +5,10 @@ from yaib.irc_event import IrcEvent
 class IrcEventTests(unittest.TestCase):
     
     def setUp(self):
-        test_message = {'content': 'hello world', 'nick': 'Waltsu', 'channel': '#testserver'}
+        test_message = {'content': 'hello world', 'type': 'PRIVMSG', 'channel': '#testserver', 'server': 'Waltsu!waltsu@waltsu.fi'}
         self.event = IrcEvent(test_message)
 
-    def test_ircflow_initialization(self):
+    def test_ircevent_initialization(self):
         self.assertEquals(self.event._server_message['channel'], '#testserver')
 
     def test_send_to_channel(self):
