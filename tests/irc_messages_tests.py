cb8c4a49f3a7c219eae091fd6e7c3865c9a453e9
Valtteri Virtanen <valtteri@taiste.fi>
2012-12-28 23:36:46 +0200

    Added test for pong message

--- tests/irc_messages_tests.py
+++ tests/irc_messages_tests.py
@@ -1,9 +1,13 @@
 import unittest
 
-from yaib.irc_messages import PrivateMessage
+from yaib.irc_messages import PrivateMessage, PongMessage
 
 class IrcMessagesTests(unittest.TestCase):
 
     def test_private_message(self):
         private_message = PrivateMessage('#channel', 'some message')
         self.assertEquals(private_message.get_command(), 'PRIVMSG #channel :some message')
+
+    def test_pong_message(self):
+        pong_message = PongMessage(1234)
+        self.assertEquals(pong_message.get_command(), 'PONG :1234')
